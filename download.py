1ce870a7144b8066d2bc71a3cbcc88370714a8db
Francesco Forcellato <francesco.forcellato@gmail.com>
2024-04-09 15:42:05 +0200

    Option to download the entire dataset publicly available

--- download.py
+++ download.py
@@ -25,9 +25,18 @@ def main() -> None:
         "title.basics.tsv.gz",
         "title.ratings.tsv.gz"
     ]
+    # datasets = [
+    #     "name.basics.tsv.gz",
+    #     "title.akas.tsv.gz",
+    #     "title.basics.tsv.gz",  # necessary
+    #     "title.crew.tsv.gz",
+    #     "title.episode.tsv.gz",
+    #     "title.principals.tsv.gz",
+    #     "title.ratings.tsv.gz"  # necessary
+    # ]
 
     os.makedirs(OUT_DIR, exist_ok=True)
-    
+
     print("Start")
     with Pool(len(datasets)) as pool:
         pool.starmap(
