cb314146fcd0b8dcebe6e169bf3e6d9758da659d
Visco01 <visconti373@gmail.com>
2024-04-12 14:52:22 +0200

    Remove duplicated migration

--- rails-movies/db/migrate/20240412092212_create_join_table_title_akas_types_new.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableTitleAkasTypesNew < ActiveRecord::Migration[7.1]
-  def change
-    create_join_table :title_akas, :types do |t|
-      t.index [:title_aka_id, :type_id]
-      t.index [:type_id, :title_aka_id]
-    end
-  end
-end
