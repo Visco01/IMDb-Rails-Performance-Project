e7309bdebe691bba48a6ccc9da8f3ea3d8c340f0
Francesco Forcellato <francesco.forcellato@gmail.com>
2024-04-19 18:53:52 +0200

    Renamed alias. Didn't make sens before

--- rails-movies/shell/add_rails_movie_alias.sh
+++ rails-movies/shell/add_rails_movie_alias.sh
@@ -1,7 +1,7 @@
 # RUN ONLY ONCE PER MACHINE!!!
 
 # Add rails-movie alias to .bashrc file
-echo 'alias rails-movie="docker exec -it rails-app bash"' >> $HOME/.bashrc
+echo 'alias rails-app="docker exec -it rails-app bash"' >> $HOME/.bashrc
 
 # Execute .bashrc to effectively add the aliases
 source $HOME/.bashrc
