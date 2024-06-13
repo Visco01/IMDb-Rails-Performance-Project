bacd7c641c95b118955f896dfffa39142e2d7966
Francesco Forcellato <francesco.forcellato@gmail.com>
2024-06-13 11:26:52 +0200

    Added ordering by rating on previous queries. Added search by genre query

--- raw-queries/search_by_director.sql
+++ raw-queries/search_by_director.sql
@@ -1,7 +1,9 @@
-select tb.primary_title, tb.original_title, tb.is_adult 
+select tb.primary_title, tb.original_title, tb.is_adult, tr.average_rating, tr.num_votes 
 from name_basics as nb 
 join directors as d on d.name_basic_id = nb.nconst
 join title_crews as tc on tc.id = d.title_crew_id 
 join title_basics as tb on tb.tconst = tc.tconst 
+join title_ratings as tr on tb.tconst = tr.tconst
 where nb.primary_name ilike '%Steven Spielberg%'
+order by tr.num_votes desc, tr.average_rating desc 
 ;
