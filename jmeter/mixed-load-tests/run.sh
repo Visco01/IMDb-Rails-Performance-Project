d14c12e816c760e9600cedd66f43ee59ee876a8f
Visco01 <visconti373@gmail.com>
2024-06-13 09:49:01 +0200

    Fix mixed-load-tests and add instructions

--- jmeter/mixed-load-tests/run.sh
+++ jmeter/mixed-load-tests/run.sh
@@ -2,8 +2,4 @@
 rm -rf ./results/*
 jmeter -n -t "./MoviesRailsMixedTests.jmx" -l "./results/movies_rails_mixed_tests.csv"
 rm -rf ./report/*
-jmeter -g "./results/title_basics_by_title.csv" -o "./report/title_basics_by_title"
-jmeter -g "./results/title_basics_by_params.csv" -o "./report/title_basics_by_params"
-jmeter -g "./results/directors_by_name.csv" -o "./report/directors_by_name"
-jmeter -g "./results/actors_by_name.csv" -o "./report/actors_by_name"
 jmeter -g "./results/movies_rails_mixed_tests.csv" -o "./report/mixed_tests"
