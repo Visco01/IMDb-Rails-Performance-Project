2ea09d875c1aed7f7602700e2c84fa4626bef3ba
Francesco Forcellato <francesco.forcellato@gmail.com>
2024-06-13 12:24:56 +0200

    Refactored queries with all the possibilities for filtering

---
+++ raw-queries/alphabetic-order/search_by_director.sql
@@ -0,0 +1,8 @@
+select tb.primary_title, tb.original_title, tb.is_adult
+from name_basics as nb 
+join directors as d on d.name_basic_id = nb.nconst
+join title_crews as tc on tc.id = d.title_crew_id 
+join title_basics as tb on tb.tconst = tc.tconst
+where nb.primary_name ilike '%Steven Spielberg%'
+order by tb.primary_title asc
+;
