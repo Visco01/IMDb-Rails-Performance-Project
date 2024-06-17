1e3428cb36852827000c3ba97fb16d089820ba8d
Visco01 <visconti373@gmail.com>
2024-06-17 10:54:14 +0200

    Add ruby prof on actors controller

--- rails-movies/app/controllers/actors_controller.rb
+++ rails-movies/app/controllers/actors_controller.rb
@@ -1,12 +1,21 @@
 class ActorsController < ApplicationController
   def index
-    page = params[:page] || 1
-    name = params[:name]
-    @actors = NameBasic.joins(:professions)
-                       .where('professions.name IN (?)', %w[actor actress])
-    @actors = @actors.where('primary_name LIKE ?', "%#{name}%") if name.present?
-    @actors = @actors.paginate(page: page, per_page: 15)
+    profile = RubyProf.profile do
+      page = params[:page] || 1
+      name = params[:name]
+      # SWAP queries!!!
+      @actors = NameBasic.joins(:professions)
+                  .where('professions.name IN (?)', %w[actor actress])
+      @actors = @actors.where('primary_name LIKE ?', "%#{name}%") if name.present?
+      @actors = @actors.paginate(page: page, per_page: 15)
 
-    render json: @actors, each_serializer: ActorSerializer
+      render json: @actors, each_serializer: ActorSerializer
+
+    end
+
+    printer = RubyProf::GraphHtmlPrinter.new(profile)
+    File.open("ruby-prof-reports/actors_profile.html", "w") do |file|
+      printer.print(file)
+    end
   end
 end
