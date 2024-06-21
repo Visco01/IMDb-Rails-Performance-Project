e696db4b1b906aae14f5d1cb7e73eb6112765550
Francesco Forcellato <francesco.forcellato@gmail.com>
2024-06-21 16:29:59 +0200

    Removed useless folder

--- jmeter/medium-load-tests-optimized/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-rm -rf ./results/*
-jmeter -n -t "./MediumTest.jmx" -l "./results/medium_test.csv"
-rm -rf ./report/*
-jmeter -g "./results/medium_test.csv" -o "./report/medium_tests"
