c7b1d46842649d2f99c9bf67ba8d4449ebcff149
Francesco Forcellato <francesco.forcellato@gmail.com>
2024-06-23 13:28:25 +0200

    mixed tests 100 users

--- jmeter/mixed-load-tests/run.sh
+++ jmeter/mixed-load-tests/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 rm -rf ./results/*
 jmeter -n -t "./MoviesRailsMixedTests.jmx" -l "./results/movies_rails_mixed_tests.csv"
+echo "End of tests. Calculating reports."
 rm -rf ./report/*
 jmeter -g "./results/movies_rails_mixed_tests.csv" -o "./report/mixed_tests"
